307df662f670a664a808c758682e26e22c62c3bd
saurabhs1000 <38377288+saurabhs1000@users.noreply.github.com>
2020-10-05 15:21:37 +0530

    Update xg_boost.py

--- Churn_modeling/xg_boost.py
+++ Churn_modeling/xg_boost.py
@@ -1,6 +1,6 @@
 # XGBoost
 
-# Importing the libraries
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -25,11 +25,10 @@ ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remaind
 X = np.array(ct.fit_transform(X))
 print(X)
 
-# Splitting the dataset into the Training set and Test set
+
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Training XGBoost on the Training set
 from xgboost import XGBClassifier
 classifier = XGBClassifier()
 classifier.fit(X_train, y_train)
